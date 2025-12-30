2860783e0ff4cfccfac597a945e1e2f4edcc0787
foraiandyt-byte <foraiandyt@gmail.com>
2025-12-30 23:30:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,30 @@ import streamlit as st
 st.header("poetican by harris")
 st.subheader("top poems")
 
-st.text("looking at the sky,
-         makes anyone cry.
+st.text("""
+looking at the sky,
+makes anyone cry.
+
+in a realm we live,
+one would never give
+up their dream
+but it seem
          
-         in a realm we live,
-         one would never give
-         up their dream
-         but it seem
+like some does
+to avoid fuss
          
-         like some does
-         to avoid fuss
+achievement is a gift,
+that would one day drift.
          
-         achievement is a gift,
-         that would one day drift.
+to achieve our goal,
+we must do with our soul.
          
-         to achieve our goal,
-         we must do with our soul.
+face our problem brave,
+with some rave.
          
-         face our problem brave,
-         with some rave.
+life is short,
+but we have time to sort
          
-         life is short,
-         but we have time to sort
-         
-         issues and fufillness,
-         to have some happiness
-")
+issues and fufillness,
+to have some happiness
+""")
