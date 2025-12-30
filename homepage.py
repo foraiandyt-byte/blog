3da6ad7f7f2e58b1f57b8d8a16103bd919369fd5
foraiandyt-byte <foraiandyt@gmail.com>
2025-12-30 23:40:41 +0530

    Update homepage layout and remove poem
    Removed poem text and updated subheader.

--- homepage.py
+++ homepage.py
@@ -1,33 +1,7 @@
 import streamlit as st
 
 st.header("poetican by harris")
-st.subheader("top poems")
-
-st.text("""
-looking at the sky,
-makes anyone cry.
-
-in a realm we live,
-one would never give
-
-up their dream
-but it seem
-         
-like some does
-to avoid fuss
-         
-achievement is a gift,
-that would one day drift.
-         
-to achieve our goal,
-we must do with our soul.
-         
-face our problem brave,
-with some rave.
-         
-life is short,
-but we have time to sort
-         
-issues and fufillness,
-to have some happiness
-""")
+st.subheader("select a poet to see their poems")
+st.set_page_config(
+         page_title="Select a poet"
+st.sidebar.success
